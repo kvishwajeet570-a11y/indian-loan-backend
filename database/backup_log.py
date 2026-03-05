3cc6bcebfb888b3a83ae1d830894c84af9cf3f79
Vishwajeet <kvishwajeet570@gmail.com>
2026-03-05 16:54:20 +0530

    Fixed backup_log and security_log models

--- database/backup_log.py
+++ database/backup_log.py
@@ -1,2 +1,29 @@
+from database.db import get_db
+from datetime import datetime
 
-from database.db import get_db from datetime import datetime     def get_backup_collection():     from database.db import get_db     db = get_db()     db = get_db()     return db["backup_logs"]    def save_backup_log(filename):      backup_collection.insert_one({          "file": filename,          "date": datetime.utcnow(),          "status": "success"      })   def get_backup_logs():      return list(          backup_collection.find(              {},              {"_id": 0}          )      )
+db = get_db()
+
+backup_collection = db["backup_logs"]
+
+
+def get_backup_collection():
+    return backup_collection
+
+
+def save_backup_log(filename):
+
+    backup_collection.insert_one({
+        "file": filename,
+        "date": datetime.utcnow(),
+        "status": "success"
+    })
+
+
+def get_backup_logs():
+
+    return list(
+        backup_collection.find(
+            {},
+            {"_id": 0}
+        ).sort("date", -1)
+    )
